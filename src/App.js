d47327f103c4c2c947531b0276966fb7d6adebf9
Ravi Shanker Tiwari <ravi.rs.tiwari@gmail.com>
2019-11-12 14:46:11 +0530

    Adding base project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi I am react app.</h1>
+      <Person></Person>
+    </div>
+  );
+}
+
+export default App;
