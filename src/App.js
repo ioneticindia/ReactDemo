0136b8cee2633b04d18b94073fc1ad7406798ad5
Ravi Shanker Tiwari <ravi.rs.tiwari@gmail.com>
2019-11-12 17:02:10 +0530

    Added Spread operator

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Person from './Person/Person';
 
 const App = () => {
-  const [ personsState, setPersonsState ] = useState({
+  const [personsState, setPersonsState] = useState({
     persons: [
       {
         name: 'Ravi',
@@ -19,11 +19,21 @@ const App = () => {
       }
     ]
   })
-  
-  const switchNameHandler = () => {
+
+  const [showPersonsState, setShowPersonsState] = useState({
+    isVisible: false
+  })
+
+  const deletePersonHandler = (index) => {
+    const persons = [...personsState.persons];
+    persons.splice(index, 1);
+    setPersonsState({ persons });
+  }
+
+  const nameChangedHandler = (evt) => {
     let persons = [
       {
-        name: 'Ankit',
+        name: evt.target.value,
         age: 28
       },
       {
@@ -39,14 +49,47 @@ const App = () => {
         age: 26
       }
     ]
-    setPersonsState({persons});
+    setPersonsState({ persons });
   }
-  
+
+  const style = {
+    backgroundColor: 'White',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
+  const togglePersonsHandler = () => {
+    let isVisible = showPersonsState.isVisible;
+    setShowPersonsState({ isVisible: !isVisible });
+  }
+
+  let persons = null
+
+  if (showPersonsState.isVisible) {
+    persons = (
+      <div>
+        {
+          personsState.persons.map((person, index) => {
+            return <Person
+              name={person.name}
+              click={deletePersonHandler.bind(this, index)}
+              age={person.age}>My hobby is Racing.</Person>
+          })}
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <h1>Hi I am react app.</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My hobby is Racing.</Person>
+      <h1>Hi, I'm React Demo App.</h1>
+      <button
+        style={style}
+        onClick={togglePersonsHandler}>Toggle Persons</button>
+      {
+        showPersonsState.isVisible ? persons : null
+      }
     </div>
   );
 }
